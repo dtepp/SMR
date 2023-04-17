4e05e05e7d62fe052ade526ecb928a8797804dac
duwenlei <750811582@qq.com>
2023-04-17 18:42:55 +0800

    method without using ai

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 import pandas as pd
 from flask import Flask, render_template, redirect, url_for, flash, request, abort
 from flask_bootstrap import Bootstrap
@@ -393,7 +395,76 @@ with app.app_context():
                 db.session.commit()
 
 
-        return "you have imported the selected school"
+    @app.route('/webhook', methods=['POST'])
+    def webhook():
+        req = request.get_json(silent=True, force=True)
+        print('Request:')
+        print(json.dumps(req, indent=4))
+        resp_text = ""
+        # Extract parameters from the request
+        intent_name = req["queryResult"]["intent"]["displayName"]
+        if intent_name == 'University':
+            school_name = req['queryResult']['parameters']['university']
+            data['school_name'] = school_name
+            resp_text = "What is your GPA?"
+        if intent_name == 'GPA':
+            gpa = req['queryResult']['parameters']['gpa']
+            data['gpa'] = gpa
+            resp_text = "What‘s your Language score? Please enter your IELTS or TOEFL directly."
+        if intent_name == 'Language scores':
+            toefl = req['queryResult']['parameters']['language']
+            ielts = req['queryResult']['parameters']['ielts']
+            if ielts is not None:
+                data['IELTS'] = ielts
+            if toefl is not None:
+                data['TOEFL'] = toefl
+            resp_text = "Which country you want to presume your postgraduate degree?"
+        if intent_name == 'Country':
+            country = req['queryResult']['parameters']['country']
+            data['country'] = country
+            resp_text = "Which filed you want to study? Please choose from these research areas: Computer Science, Engineering, Business, Economics, Law, Medicine, Arts, Education, Social Science, Agriculture, Environment. Please describe the details the courses you want to learn from the field you choose: For example, if you choose Computer Science, you can enter 'Computer Science: Machine Learning, Deep Learning, Computer Vision, Natural Language Processing, Data Mining, Data Science, Big Data, etc.'"
+        if intent_name == 'Research area':
+            research_area = req['queryResult']['queryText']
+            result = research_area.split(":")
+            data['research_area'] = result[0]
+            data['courses'] = result[1]
+            # 在这里，调取杜杜的方法，将json传到后端，去进行下一步操作
+            resp_text = "Here are the universities you can apply for."
+
+        # Save data to file in JSON format
+
+        with open('data.json', 'w') as f:
+            json.dump(data, f)
+
+        return make_response(jsonify({'fulfillmentText': resp_text}))
+
+
+    def handleRequest(request:json):
+        school = request['school_name']
+        gpa = request['gpa']
+        ielts = request.get('IELTS', 0)
+        toefl = request.get('TOEFL', 0)
+        country = request['country']
+        schoolInDb = SchoolLevel.query.filter(schoolname = school).first()
+        finalGpa = int(gpa) + int(schoolInDb.addscore)
+
+        allSchool =  SchoolLevel.query.filter( (SchoolLevel.country == country) & (SchoolLevel.isapply == True )).all() # country
+        gpaGoodScool = []
+        for school in allSchool:
+            requireScore =  int (school.schoolscore)
+            if requireScore<finalGpa:  #  gpa
+                gpaGoodScool.append(school)
+
+
+        languageResult = languageFilter(ielts, toefl)  # language
+        finalResult = []
+        for major in languageResult:
+            if major.school in gpaGoodScool:
+                finalResult.append(major)
+
+
+
+        return finalResult
 
     if __name__ == "__main__":
         db.create_all()
